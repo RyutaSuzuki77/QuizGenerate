068094ecc2c358a817ef24062d44f0c94d2be96b
suzukir <ryuta.bump@gmail.com>
2024-09-03 23:37:31 +0900

    サンプルページ作成

---
+++ resources/js/Pages/Sample.jsx
@@ -0,0 +1,12 @@
+import { Link, Head } from '@inertiajs/react';
+
+export default function Sample({}) {
+    return (
+        <>
+            <Head title="Sample" />
+            <div>
+                TEST
+            </div>
+        </>
+    );
+}
